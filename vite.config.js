a6a88728ff13602481302746d84828b2cbee92b6
MuzzammilN <naqvimuzzammil@gmail.com>
2025-11-08 13:22:07 -0500

    some changes

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
   build: {
     outDir: 'build',
   },
 });
-=======
-    build: {
-    outDir: 'build'
-  },
-})
->>>>>>> 3b24af39d87c28506aa919c7b868386524ed3f99
